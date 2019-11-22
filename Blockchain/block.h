5272bc658e98be43b7732b8e24b1fc034f9c2054
abelzis <abelzis@hotmail.com>
2019-11-22 12:09:49 +0200

    Version v2.0.1 - Added libbitcoin

--- Blockchain/block.h
+++ Blockchain/block.h
@@ -9,6 +9,12 @@
 #include "transaction.h"
 using std::vector;
 
+// USING libbitcoin
+//#include <bitcoin/bitcoin.hpp>
+
+// for release build - comment this define
+//#define TEST_BUILD
+
 
 bool isVerified_(const Transaction& tx) { return tx.getVerification(); }
 
@@ -20,7 +26,11 @@ private:
 	unsigned int nonce_;
 	vector<Transaction> tx_list_;
 
+#ifdef TEST_BUILD
+	string setDifficultyTarget_() { return "0"; }
+#else
 	string setDifficultyTarget_() { return "0000"; }
+#endif
 
 	// payments has to be legal - you cant send 120 coins if you have 100
 	// and if hashID is the same, it verifies that nothing has been changed in the transaction
@@ -88,6 +98,18 @@ public:
 	void setMerkleRoot() {
 		tx_list_.shrink_to_fit();
 
+		// from bitcoin
+		if (tx_list_.empty())
+		{
+			merkle_root_ = Hash("").getHash();
+			return;
+		}
+		else if (tx_list_.size() == 1)
+		{
+			merkle_root_ = Hash(tx_list_[0].getAsString()).getHash();
+			return;
+		}
+
 		vector<Hash> hash_list;
 		hash_list.resize(tx_list_.size() / 2 + 1);
 		//hash_list.reserve(tx_list_.size() / 2 + 1);	//set initial size to half of transaction quantity
@@ -99,19 +121,23 @@ public:
 			if (i + 1 < tx_list_.size())
 				hash_list[i / 2] = Hash(tx_list_[i].getAsString() + tx_list_[i + 1].getAsString());
 			else
-				hash_list[i / 2] = Hash(tx_list_[i].getAsString());
+				hash_list[i / 2] = Hash(tx_list_[i].getAsString() + tx_list_[i].getAsString());		// if amount is odd, duplicate last transaction
 		}
 
 		
 		// hashing hashes together. Goal: 1 single hash from any number
 		while (hash_list.size() > 1)
 		{
-			if (hash_list.size() == 3)
+			/*if (hash_list.size() == 3)
 			{
 				hash_list[0] = Hash(Hash(hash_list[0].getHash() + hash_list[1].getHash()).getHash() + hash_list[2].getHash());
 				hash_list.resize(1);
 				break;
-			}
+			}*/
+
+			// if hash amount is odd, duplicate last hash
+			if (hash_list.size() % 2 != 0)
+				hash_list.push_back(hash_list.back());
 
 			int index = 0;
 			while (index < hash_list.size())
@@ -122,11 +148,11 @@ public:
 					hash_list[index / 2] = Hash(hash_list[index].getHash() + hash_list[index + 1].getHash());
 					//hash_list.resize(index / 2);
 				}
-				else
-				{
-					hash_list[index / 2] = Hash(hash_list[index].getHash());
-					//hash_list_.resize()
-				}
+				//else
+				//{
+				//	hash_list[index / 2] = Hash(hash_list[index].getHash());
+				//	//hash_list_.resize()
+				//}
 				index += 2;
 			}
 			hash_list.resize(hash_list.size() / 2);
